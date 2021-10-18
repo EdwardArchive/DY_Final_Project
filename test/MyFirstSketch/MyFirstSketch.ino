1c9f2a42fc13197334dd292e07a43b257a643f06
Edward <kjjs1996@gmail.com>
2021-10-18 10:14:46 +0900

    - test Pyduino - fix main UI - fix widget UI - add/test temp complie button

--- test/MyFirstSketch/MyFirstSketch.ino
+++ test/MyFirstSketch/MyFirstSketch.ino
@@ -1,11 +1,16 @@
+
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
+    Serial.println("ON1");
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
+    delay(500);
+
+    Serial.println("OFF1");
     digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    delay(500);
 }
 
--- test/qt_test1/main.py
+++ test/qt_test1/main.py
@@ -1,17 +1,21 @@
 import sys
 import os
+import pyduinocli
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-## 차후 Path 설정에 대한것은 리눅스 윈도우 동일한 방법으로 진행할수 있도록 고민이 필요 현제 windows 버전
+## Pyduinocli PATH
+arduino = pyduinocli.Arduino("/home/kbj/local/bin/arduino-cli")
+
+## UI PATH
 print(os.getcwd())
 pathfile=os.getcwd()
 ui_path = os.path.dirname(os.path.abspath("/home/kbj/Project/test/qt_test1/ui/"))
-ui_path2 = os.path.join(pathfile,"test\qt_test1")
-form_class = uic.loadUiType(os.path.join(ui_path2, "ui\main_window.ui"))[0]
-dig_class = uic.loadUiType(os.path.join(ui_path2, "ui\widget.ui"))[0]
-drag_class = uic.loadUiType(os.path.join(ui_path2, "ui\dragdrop.ui"))[0]
+ui_path2 = os.path.join(pathfile,"test/qt_test1")
+form_class = uic.loadUiType(os.path.join(ui_path2, "ui/main_window.ui"))[0]
+dig_class = uic.loadUiType(os.path.join(ui_path2, "ui/widget.ui"))[0]
+drag_class = uic.loadUiType(os.path.join(ui_path2, "ui/dragdrop.ui"))[0]
 
 print(form_class)
 print(drag_class)
@@ -20,12 +24,28 @@ class Code_dialog(QDialog,dig_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.buttonBox.button(QDialogButtonBox.Save).clicked.connect(self.save_clicked)
+
+    def save_clicked(self):
+        arduino.compile(sketch="/home/kbj/Project/test/MyFirstSketch/MyFirstSketch.ino",fqbn="arduino:avr:uno",port="/dev/ttyACM0",clean=True,verify=True,upload=True)
+        QMessageBox.about(self,"message","complie done")
+        
+
+    def ok_callback(self):
+        print("OK")
+        self.close()
+
+    def cancel_callback(self):
+        print("Cancel")
+        self.close()
 
 class drag_dialog(QDialog,drag_class):
     
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+
 
 
 class MyWindow(QMainWindow, form_class):
@@ -36,7 +56,7 @@ class MyWindow(QMainWindow, form_class):
         self.open_btn2.clicked.connect(self.btn2_clicked)
 
     def btn_clicked(self):
-        QMessageBox.about(self,"message","clicked")
+        QMessageBox.about(self,"message","codedialog_open")
         mydilog = Code_dialog()
         mydilog.exec_()
 
